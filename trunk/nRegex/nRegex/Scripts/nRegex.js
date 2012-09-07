a876b084d044d3edc39e87f2a7f2a3327e7d3396
t3rse <david.seruyange@gmail.com>
2012-09-07 09:21:24 -0500

    Basic UI Skeleton in place, base javascript added

---
+++ trunk/nRegex/nRegex/Scripts/nRegex.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+
+    window.setInterval(function () {
+        var _rg = $("#regexEntryTextArea").val();
+        var _target = $("#regexTargetArea").val();
+        console.log("Regex: " + _rg + " / Target: " + _target);
+    }, 500);
+
+
+});
